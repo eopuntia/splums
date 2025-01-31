88628b19d6aa9e13030805a4e2c6e4152ca267a1
Clara <clamcgrew98@gmail.com>
2025-01-31 14:28:41 -0500

    Updated usercreation and models.py

--- splums/usercreation.py
+++ splums/usercreation.py
@@ -1,13 +1,69 @@
-import cam
-import datetime
+from datetime import datetime
+from main import session
+from models.models import users
 
+#*******************************************************************************************
+# CREATE NEW USERS
+#*******************************************************************************************
 
-def create_user():
+def create_user(bronco_id: str, name: str, photo_url: str, user_type_id: int):
+    created_at = datetime.now()
+  
+    new_user = users(
+        bronco_id = bronco_id,
+        name = name,
+        photo_url =photo_url,
+       user_type_id = user_type_id,
+       created_at = created_at,
+        last_updated_at=created_at,
+    )
 
-    bronco_id= input("Type the user's bronco id: ")
-    name = input("Type the user's first and last name: ")
-    photo_url = cam.take_picture(name)
-    user_type_id = int(input("What type of user is this\n(1): Administrator\n(2): Attendant\n(3): Student\n(4): Blacklisted\n"))
+    try:
+        session.add(new_user)
+        session.commit()
+        print(f"User created successfully with bronco ID: {new_user.bronco_id}")
+        return new_user.bronco_id # Return user_id for testing
+    except Exception as e:
+        session.rollback()
+        print(f"Error creating user: {e}")
+        return -1
 
+#*******************************************************************************************
+# EDIT USERS
+#*******************************************************************************************
+
+def edit_user(bronco_id: int, user: str):
+    updated_user = session.query(users).filter_by(bronco_id =bronco_id).first()
+
+    if updated_user: # Check if user exists and commit changes
+        updated_user.bronco_id = bronco_id
+        updated_user.last_updated_at = datetime.now()
+
+        session.commit()
+        print("User updated successfully.")
+        # return 1
+    else:
+        session.rollback()
+        print("user not found. Unable to edit.")
+        # return 0
+
+#*******************************************************************************************
+# DELETE USERS
+#*******************************************************************************************
+
+def delete_user(bronco_id: int):
+    deleted_user = session.query(users).filter_by(bronco_id=bronco_id).first()
+
+    if deleted_user: # Check if user exists and delete
+        session.delete(deleted_user)
+        session.commit()
+        print(f"User with ID {bronco_id} has been deleted.")
+        # return 1
+    else:
+        session.rollback()
+        print(f"User with ID {bronco_id} not found. Unable to delete.")
+        # return 0
 
 create_user()
+created_user_id = "214151f"
+edit_user(created_user_id, "Edited user!") # should be success
