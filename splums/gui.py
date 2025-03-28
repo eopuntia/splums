aa14225a600e9351a3a05c1d3c129b46f806c1f7
Renee Ricker <renee.rickert02@gmail.com>
2025-03-28 09:38:15 -0400

    reorganize gui code

--- splums/gui.py
+++ splums/gui.py
@@ -50,7 +50,6 @@ class Notes(QWidget):
         remove_button = QPushButton('Remove Note')
         remove_button.clicked.connect(self.remove_note)
         save_button = QPushButton('Save')
-        save_button.clicked.connect(self.save_to_db)
 
         self.list_widget.setStyleSheet("""
             QListWidget::item {
@@ -90,7 +89,12 @@ class Notes(QWidget):
 
 class EditAccount(QWidget):
     def __init__(self, account_table):
+        # TODO this needs to have the information filled in and editeable
+        # TODO It then needs to have a save button to confirm the changes
         super().__init__()
+
+        self.setStyleSheet("QTableWidget{font-size: 18pt;} QHeaderView{font-size: 12pt;}")
+
         self.account_table = account_table
         layout = QFormLayout()
         self.setWindowTitle("Edit Account")
@@ -101,13 +105,6 @@ class EditAccount(QWidget):
             account_id = self.account_table.item(current_account_row, 4)
             print(account_id.text())
         
-        #Make a Select statement (with SQLAlchemy and event broker?) to find in the database where the student_id is equal to the account_id of the current account
-        #Add the relevant fields from the database to the fields below with setText() instead of setPlaceholderText()
-            
-        # Style Sheet for default styling options on widgets
-        self.setStyleSheet("QTableWidget{font-size: 18pt;} QHeaderView{font-size: 12pt;}")
- 
-        # Create WIN input box
         self.win_box = QLineEdit()
         self.win_box.setPlaceholderText("WIN...")
         self.win_box.setInputMask('999999999')
@@ -116,18 +113,15 @@ class EditAccount(QWidget):
         name_regex = QRegularExpression("[A-Za-z]+")
         name_validator = QRegularExpressionValidator(name_regex)
  
-        # Create display name box
         self.display_name = QLineEdit()
         self.display_name.setPlaceholderText("Display Name...")
         self.display_name.setValidator(name_validator)
  
-        # Create given name box
         self.given_name = QLineEdit()
         self.given_name.setPlaceholderText("Given Name...")
         self.given_name.setValidator(name_validator)
         # self.given_name.textChanged.connect(self.update_win)
  
-        # Create surname box
         self.surname = QLineEdit()
         self.surname.setPlaceholderText("Last Name...")
         
@@ -136,17 +130,14 @@ class EditAccount(QWidget):
 
         self.permissions = QComboBox()
 
-        # Create affiliation box
         self.affiliation = QLineEdit()
         self.affiliation.setPlaceholderText("Affiliation...")
         self.affiliation. setValidator(name_validator)
  
-        # Create rso box
         self.rso = QLineEdit()
         self.rso.setPlaceholderText("Registered Student Org...")
         self.rso. setValidator(name_validator)
        
-        # Add fields to the form layout
         layout.addRow("WIN:", self.win_box)
         layout.addRow("Role:", self.role)
         layout.addRow("Display Name:", self.display_name)
@@ -156,7 +147,6 @@ class EditAccount(QWidget):
         layout.addRow("Affiliation:", self.affiliation)
         layout.addRow("RSO:", self.rso)
  
-        # Create button that opens camera using cam.py
         photo_button = QPushButton("Take Photo")
         photo_button.clicked.connect(self.show_photo)
 
@@ -416,9 +406,64 @@ class AddAccount(QSqlDatabase, QWidget):
         self.close()
 
 class MainWindow(QMainWindow):
-#*******************************************************************************************
-# Button Functions
-#*******************************************************************************************
+    def __init__(self, client_connection):
+        super().__init__()
+        self.client_connection = client_connection
+
+        self.setWindowTitle("Student Projects Lab User Management System")
+        self.setMinimumSize(QSize(1280, 720))
+
+        main_widget = QWidget()
+        main_widget.setObjectName("Main")
+
+        layout_main = QVBoxLayout()
+        main_widget.setLayout(layout_main)
+        self.setCentralWidget(main_widget)
+
+        # initialize as empty for now
+        self.accounts = []
+
+        button_dim=[100, 80]
+        button_icon_dim=[50, 50]
+
+        # digit width, dimensions, digit value
+        self.headcount_display = self.initialize_lcd(2, [button_dim[0]-30, button_dim[1]-25], 0)
+
+        self.add_button = self.initialize_button("Add Account", "./splums/images/add.svg", self.add_account, button_dim, button_icon_dim)
+        self.edit_button = self.initialize_button("Edit Account", "./splums/images/edit.svg", self.edit_account, button_dim, button_icon_dim)
+        self.signout_button = self.initialize_button("Sign Out", "./splums/images/signout.svg", self.sign_out, button_dim, button_icon_dim)
+
+        layout_topsplit = QHBoxLayout()
+        layout_main.addLayout(layout_topsplit)
+
+        button_bar = QHBoxLayout()
+        button_bar.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        button_bar.addWidget(self.add_button)
+        button_bar.addWidget(self.edit_button)
+        button_bar.addWidget(self.signout_button)
+        layout_topsplit.addLayout(button_bar)
+
+        topright_bar = QHBoxLayout()
+        topright_bar.setAlignment(Qt.AlignmentFlag.AlignRight)
+        headcount_label_and_lcd = QVBoxLayout()
+        headcount_header = QLabel(" Headcount")
+        headcount_header.setStyleSheet("font-weight: bold;")
+        headcount_header.setObjectName("HeadcountHeader")
+        headcount_label_and_lcd.addWidget(headcount_header)
+        headcount_label_and_lcd.addWidget(self.headcount_display)
+        topright_bar.addLayout(headcount_label_and_lcd)
+        layout_topsplit.addLayout(topright_bar)
+
+        self.account_table = self.initialize_account_table()
+
+        layout_accounts = QHBoxLayout()
+        layout_main.addLayout(layout_accounts)
+
+        layout_accounts.addWidget(self.account_table)
+
+        self.initial_accounts_load()
+        self.render_accounts_to_screen()
+
     def edit_account(self):
         self.w = EditAccount(self.account_table)
         self.w.show()
@@ -429,140 +474,43 @@ class MainWindow(QMainWindow):
     def sign_out(self):
         print("Signing out")
 
-    def __init__(self, client_connection):
-        super().__init__()
+    def initialize_button(self, title, icon_path, function, button_dim, button_icon_dim):
+        new_button = QToolButton(self)
+        new_button.setText(title)
+        new_button.setIcon(QIcon(icon_path))
+        new_button.clicked.connect(function)
+        new_button.setFixedSize(QSize(button_dim[0], button_dim[1]))
+        new_button.setIconSize(QSize(button_icon_dim[0], button_icon_dim[1]))
+        return new_button
 
-        # initialize as empty for now
-        self.accounts = []
-        # client connection
-        self.client_connection = client_connection
-        #Table containing all the accounts
-        self.account_table = QTableWidget()
-        #selection = self.account_table.selectionModel()
-        self.headcount_display = QLCDNumber(self)
-        #self.account_table.selectRow(0)
-     
-        self.setWindowTitle("Student Projects Lab User Management System")
+    def initialize_lcd(self, digit_count, button_dim, digit):
+        new_lcd = QLCDNumber(self)
+        new_lcd.setDigitCount(digit_count)
+        new_lcd.setFixedSize(QSize(button_dim[0], button_dim[1]))
+        new_lcd.display(digit)
+        return new_lcd
 
-        #Main layout, the over-arching vertical layout that The button bar and the displayed users are apart of
-        layout_main = QVBoxLayout()
+    def initialize_account_table(self):
+        new_account_table = QTableWidget()
+        new_account_table.verticalHeader().setVisible(False)
 
-        #Minimum size
-        self.setMinimumSize(QSize(1280, 720))
+        new_account_table.setSelectionBehavior(QTableView.SelectionBehavior.SelectRows)
+        new_account_table.setSelectionMode(QAbstractItemView.SelectionMode.SingleSelection)
+        new_account_table.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
+        new_account_table.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        new_account_table.setHorizontalScrollMode(QAbstractItemView.ScrollMode(0))
 
-        layout_topsplit = QHBoxLayout()
+        new_account_table.setColumnCount(5)
 
-        #*******************************************************************************************
-        # BUTTON BAR
-        #*******************************************************************************************
-        layout_buttonbar = QHBoxLayout()
-        layout_buttonbar.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        new_account_table.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeMode.ResizeToContents)
+        new_account_table.horizontalHeader().setSectionResizeMode(1, QHeaderView.ResizeMode.Stretch)
+        new_account_table.horizontalHeader().setSectionResizeMode(2, QHeaderView.ResizeMode.Stretch)
+        new_account_table.horizontalHeader().setSectionResizeMode(3, QHeaderView.ResizeMode.Stretch)
 
-        #Add Account Button
-        self.button_add = QToolButton(self)
-        self.button_add.setText("Add Account")
-        self.button_add.setIcon(QIcon("./splums/images/add.svg"))
-        self.button_add.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextUnderIcon)
-
-        self.button_add.clicked.connect(self.add_account)
-
-        #Edit Account Button
-        self.button_edit = QToolButton(self)
-        self.button_edit.setText("Edit Account")
-        self.button_edit.setIcon(QIcon("./splums/images/edit.svg"))
-        self.button_edit.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextUnderIcon)
-
-        self.button_edit.clicked.connect(self.edit_account)
-
-        #Sign Out Button
-        self.button_sign_out = QToolButton(self)
-        self.button_sign_out.setText("Sign Out")
-        self.button_sign_out.setIcon(QIcon("./splums/images/signout.svg"))
-
-        self.button_sign_out.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextUnderIcon)
-        self.button_sign_out.clicked.connect(self.sign_out)
-
-        #Set dimensions of buttonbar buttons
-        bdim=[100, 80]
-        bicondim=[50, 50]
-        self.button_add.setFixedSize(QSize(bdim[0], bdim[1]))
-        self.button_edit.setFixedSize(QSize(bdim[0], bdim[1]))
-        self.button_sign_out.setFixedSize(QSize(bdim[0], bdim[1]))
-
-        self.button_add.setIconSize(QSize(bicondim[0], bicondim[1]))
-        self.button_edit.setIconSize(QSize(bicondim[0], bicondim[1]))
-        self.button_sign_out.setIconSize(QSize(bicondim[0], bicondim[1]))
-
-        #Add the buttons to the button bar
-        layout_buttonbar.addWidget(self.button_add)
-        layout_buttonbar.addWidget(self.button_edit)
-        layout_buttonbar.addWidget(self.button_sign_out)
-
-        #Add the button bar to the main layout
-        layout_topsplit.addLayout(layout_buttonbar)
-
-        #*******************************************************************************************
-        # Head Count
-        #*******************************************************************************************
-
-        #Righthand toolbar for headcount 
-        layout_rightbar = QHBoxLayout()
-        layout_rightbar.setAlignment(Qt.AlignmentFlag.AlignRight)
-
-        layout_headcount = QVBoxLayout()
-        layout_headcount.setSpacing(0)
-        headcount_header = QLabel(" Headcount")
-        headcount_header.setStyleSheet("font-weight: bold;")
-        headcount_header.setObjectName("HeadcountHeader")
-        layout_headcount.addWidget(headcount_header)
-
-        self.headcount_display.setDigitCount(2)
-        layout_headcount.addWidget(self.headcount_display)
-        layout_rightbar.addLayout(layout_headcount)
-        layout_topsplit.addLayout(layout_rightbar)
-
-        layout_main.addLayout(layout_topsplit)
-        self.headcount_display.setFixedSize(QSize(bdim[0]-30, bdim[1]-25))
-        self.headcount_display.display(0)
-
-        #*******************************************************************************************
-        # Account Table
-        #*******************************************************************************************
-        
-        layout_accounts = QHBoxLayout()
-
-        self.account_table.verticalHeader().setVisible(False)
-
-        #Configuring way table can be interacted
-        self.account_table.setSelectionBehavior(QTableView.SelectionBehavior.SelectRows)
-        self.account_table.setSelectionMode(QAbstractItemView.SelectionMode.SingleSelection)
-        self.account_table.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
-        self.account_table.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
-        self.account_table.setHorizontalScrollMode(QAbstractItemView.ScrollMode(0))
-
-        #Setting up columns
-        self.account_table.setColumnCount(5)
-
-        #Automatic handling of resizing window for table
-        self.account_table.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeMode.ResizeToContents)
-        self.account_table.horizontalHeader().setSectionResizeMode(1, QHeaderView.ResizeMode.Stretch)
-        self.account_table.horizontalHeader().setSectionResizeMode(2, QHeaderView.ResizeMode.Stretch)
-        self.account_table.horizontalHeader().setSectionResizeMode(3, QHeaderView.ResizeMode.Stretch)
         column_labels = [" ", "Account", "Permissions", "Notes", "Head Count"]
+        new_account_table.setHorizontalHeaderLabels(column_labels)
 
-        self.account_table.setHorizontalHeaderLabels(column_labels)
-
-        self.initial_accounts_load()
-        self.render_accounts_to_screen()
-
-        layout_accounts.addWidget(self.account_table)
-
-        layout_main.addLayout(layout_accounts)
-
-        widget = QWidget()
-        widget.setObjectName("Main")
-        widget.setLayout(layout_main)
-        self.setCentralWidget(widget)
+        return new_account_table
 
     def initial_accounts_load(self):
         get_account_event = Event(event_type=EventTypes.GET_USERS_BY_ROLE, data = {'role': ''})
@@ -603,10 +551,10 @@ class MainWindow(QMainWindow):
 #            account_permissions_cell.setAlignment(Qt.AlignmentFlag.AlignCenter)
 #            self.account_table.setCellWidget(row, 2, account_permissions_cell)
 
+            # horizontal row of buttons for each note this is the layout for the actual notewidget
             note_layout = QHBoxLayout()
-            print(acc.notes)
             for note in acc.notes:
-                print(note)
+                # each note gets a button that shows its text
                 note_button = QPushButton()
                 note_button.setText(note)
                 note_button.setFixedSize(QSize(60, 60))
