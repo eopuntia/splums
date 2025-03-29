5c133892aabac754f450fe1f05bdefb6724a6f0e
Evan-Handy <112645052+Evan-Handy@users.noreply.github.com>
2025-03-29 19:54:56 -0400

    Update LabGUI.py
    Lab Screen v1.3

--- LabGUI.py
+++ LabGUI.py
@@ -3,10 +3,16 @@ import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QToolButton, QTableWidget, \
     QTableWidgetItem, QTableView, QAbstractItemView, QLabel, QHeaderView, QSizePolicy
 from PyQt6.QtCore import Qt, QSize
-from PyQt6.QtGui import QPixmap, QIcon
+from PyQt6.QtGui import QPixmap, QIcon, QFont
+import math
 
 
-
+#ADD TIME, affiliation ,RSOs
+#     PIC           Time
+#     NAME
+#     AFFILIATION
+#     RSOs
+#     PERMISSIONS
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -21,46 +27,58 @@ class MainWindow(QMainWindow):
         # Make GUI Vertical
         self.setMinimumSize(QSize(720, 1280))
 
-        #Button Location
-        layout_button = QHBoxLayout()
-        layout_button.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        self.users = [{"User Name": "Estlin Mendez","Affiliation": "Creator", "RSOs": "Test", "Permissions": "Red", "Notes": "", "url": "temp.png", "Time": "12:30"},
+                 {"User Name": "Clara McGrew", "Affiliation": "Creator", "RSOs": "", "Permissions": "Red", "Notes": "", "url": "temp.png", "Time": ""},
+                 {"User Name": "Renee Rickert","Affiliation": "Creator", "RSOs": "", "Permissions": "Red", "Notes": "", "url": "temp.png", "Time": ""},
+                 {"User Name": "Evan Handy","Affiliation": "Creator", "RSOs": "", "Permissions": "Green", "Notes": "", "url": "temp.png", "Time": "" },
+                 {"User Name": "Hunter Hamrick","Affiliation": "Creator", "RSOs": "", "Permissions": "Blue", "Notes": "", "url": "temp.png", "Time": ""},
+                 {"User Name": "Kaden Kramer", "Affiliation": "Creator", "RSOs": "", "Permissions": "Blue", "Notes": "", "url": "temp.png", "Time": ""},
+                 {"User Name": "Ben Crane","Affiliation": "Creator", "RSOs": "", "Permissions": "Blue", "Notes": "", "url": "temp.png", "Time": ""},
+                 {"User Name": "Jerry Sims","Affiliation": "Faux Attendant", "RSOs": "", "Permissions": "Blue", "Notes": "", "url": "temp.png", "Time": ""}]
 
-        #Remove Student Button
-        self.button_remove = QToolButton(self)
-        self.button_remove.setText("Remove Student")
-        #ADD ICON?
-        #self.button_remove.setIcon()
-        self.button_remove.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextUnderIcon)
-        self.button_remove.clicked.connect(self.remove_user)
-        layout_button.addWidget(self.button_remove)
-        layout_main.addLayout(layout_button)
+
+
 
         #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
         # Table for the Cards
-        layout_lab = QVBoxLayout()
+        layout_lab = QHBoxLayout()
         #Users currently in the lab
         self.lab_table = QTableWidget()
 
         self.lab_table.verticalHeader().setVisible(False)
+        self.lab_table.setStyleSheet("QTableWidget::item:selected {background-color: transparent;}")
 
-        self.lab_table.setSelectionBehavior(QTableView.SelectionBehavior.SelectItems)
-        self.lab_table.setSelectionMode(QAbstractItemView.SelectionMode.SingleSelection)
-        self.lab_table.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
-        self.lab_table.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAsNeeded)
-        self.lab_table.setHorizontalScrollMode(QAbstractItemView.ScrollMode(0))
+        #Bold Headers
+        bold_font = QFont()
+        bold_font.setBold(True)
 
-        self.lab_table.setColumnCount(1)
-        self.lab_table.setHorizontalHeaderLabels(["Student Card"])
+        self.lab_table.setColumnCount(5)
+        self.lab_table.setHorizontalHeaderLabels(["Users Present", "", "", "Head Count", str(len(self.users) - 1)])
+
+        header = self.lab_table.horizontalHeader()
+        header.setFont(bold_font)
 
         self.lab_table.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         self.update_table()
+        #Allow Window Resizing
+        self.lab_table.horizontalHeader().setStretchLastSection(True)
+        #Allow all columns to stretch
+        for col in range(self.lab_table.columnCount()):
+            self.lab_table.horizontalHeader().setSectionResizeMode(col, QHeaderView.ResizeMode.Stretch)
+
+        #Disable Editing
+        self.lab_table.setSelectionMode(QAbstractItemView.SelectionMode.NoSelection)
+        self.lab_table.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
+        self.lab_table.setFocusPolicy(Qt.FocusPolicy.NoFocus)
 
         layout_lab.addWidget(self.lab_table)
         layout_lab.setStretch(0,1)
 
+
         layout_main.addLayout(layout_lab)
         self.lab_table.resizeRowsToContents()
-        self.lab_table.resizeColumnToContents(0)
+        self.lab_table.resizeColumnsToContents()
+
         widget = QWidget()
         widget.setLayout(layout_main)
         self.setCentralWidget(widget)
@@ -69,49 +87,98 @@ class MainWindow(QMainWindow):
         card = QWidget()
         card_layout = QVBoxLayout()
 
-        #Student Image
+        #TimeLayout
+        time_layout =QHBoxLayout()
+        #Time Swiped In
+        time = QLabel((user['Time']))
+        time.setAlignment(Qt.AlignmentFlag.AlignRight)
+        time_layout.addWidget(time)
+
+        #User Image
         user_img = QLabel()
-        user_img.setPixmap(QPixmap("./splums/images/default.png").scaledToHeight(85))
+        user_img.setPixmap(QPixmap("./images/").scaledToHeight(85))
         card_layout.addWidget(user_img)
-        #Student Name
-        user_name = QLabel(user['Student Name'])
+
+        #User Name
+        user_name = QLabel(user['User Name'])
         user_name.setAlignment(Qt.AlignmentFlag.AlignCenter)
         user_name.setStyleSheet("font-size: 16pt")
         card_layout.addWidget(user_name)
 
+        #Affiliation
+        user_affiliation = QLabel(user['Affiliation'])
+        user_affiliation.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        user_affiliation.setStyleSheet("font-size: 16pt")
+        card_layout.addWidget(user_affiliation)
+
+        #RSOs
+        user_rso = QLabel(user['RSOs'])
+        user_rso.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        user_rso.setStyleSheet("font-size: 16pt")
+        card_layout.addWidget(user_rso)
+
         #Permissions
-        user_permissions = QLabel(f"Permissions: {user['Permissions']}")
+        user_permissions = QLabel(f"{user['Permissions']}")
         permission_stylesheet = f"color: {user['Permissions'].lower()}; font-size: 14pt;"
         user_permissions.setStyleSheet(permission_stylesheet)
         user_permissions.setAlignment(Qt.AlignmentFlag.AlignCenter)
         card_layout.addWidget(user_permissions)
+        #Add time to top of card
+        card_layout.insertLayout(0, time_layout)
+        card.setLayout(card_layout)
+        return card
 
+    def attendant_card(self, user):
+        card = QWidget()
+        card_layout = QVBoxLayout()
+
+        # Attendant Image
+        user_img = QLabel()
+        user_img.setPixmap(QPixmap("./splums/images/default.png").scaledToHeight(85))
+        card_layout.addWidget(user_img)
+        # Attendant Name
+        user_name = QLabel(user['User Name'])
+        user_name.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        user_name.setStyleSheet("font-size: 16pt")
+        card_layout.addWidget(user_name)
+
+        # Affiliation
+        user_affiliation = QLabel(user['Affiliation'])
+        user_affiliation.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        user_affiliation.setStyleSheet("font-size: 16pt")
+        card_layout.addWidget(user_affiliation)
+
+        card.setMaximumHeight(180)
         card.setLayout(card_layout)
         return card
 
     def update_table(self):
-        users = [{"Student Name": "Estlin Mendez", "Permissions": "Red", "Notes": "", "url": "temp.png"},
-                {"Student Name": "Clara McGrew", "Permissions": "Red", "Notes": "", "url": "temp.png"},
-                {"Student Name": "Renee Rickert", "Permissions": "Red", "Notes": "", "url": "temp.png"},
-                {"Student Name": "Evan Hardy", "Permissions": "Green", "Notes": "", "url": "temp.png", },
-                {"Student Name": "Hunter Hamrick", "Permissions": "Blue", "Notes": "", "url": "temp.png"},
-                {"Student Name": "Kaden Kramer", "Permissions": "Blue", "Notes": "", "url": "temp.png"},
-                {"Student Name": "Ben Crane", "Permissions": "Blue", "Notes": "", "url": "temp.png"}]
-        self.lab_table.setRowCount(len(users))
+        non_attendant = self.users[:-1]
+        self.lab_table.setRowCount(math.ceil(len(non_attendant)/6))
 
+        column = 0
         row = 0
         #Add User Card to the table on the screen
-        for user in users:
+        for user in non_attendant:
             card = self.user_card(user)
-            self.lab_table.setCellWidget(row, 0, card)
-            row += 1
+            self.lab_table.setCellWidget(row, column, card)
+            self.lab_table.resizeColumnToContents(column)
+            column += 1
+            #After 5th user move to next row
+            if column > 4:
+                column = 0
+                row += 1
 
-    def remove_user(self):
-        print("Removing user...")
+        attendant_card_widget = self.attendant_card(self.users[-1])
+        self.lab_table.setCellWidget(row, 4, attendant_card_widget)
 
-# Might need to move this into main
+
+        # Resize columns and rows to fit the content
+        self.lab_table.resizeColumnsToContents()
+        self.lab_table.resizeRowsToContents()
+
 splums = QApplication(sys.argv)
-splums.setStyle("Breeze")
+
 window = MainWindow()
 window.show()
 splums.exec()
